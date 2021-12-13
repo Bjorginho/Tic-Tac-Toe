4ec8a28e7c2066fdc6851095ed1253fdf3f60280
Andre Bjørgum <andre.bjorgum@student.uib.no>
2021-12-13 18:50:41 +0100

    Initial commit

---
+++ src/Game/TicTacToe.java
@@ -0,0 +1,15 @@
+package Game;
+
+import javax.swing.*;
+
+public class TicTacToe {
+    JFrame frame;
+
+    public TicTacToe() {
+        this.frame = new JFrame("Tic Tac Toe");
+    }
+
+    public void start(){
+
+    }
+}
